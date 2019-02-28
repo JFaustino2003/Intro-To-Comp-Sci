5210efb77775a3be8a8c96680ef431da37760203
jfaustino <jfaustino@HS-D210-19.MOHS.local>
2019-02-28 14:20:09 -0500

    2/28/19

--- Lesson 3 - Strings/src/StringsAssignment.java
+++ Lesson 3 - Strings/src/StringsAssignment.java
@@ -26,8 +26,21 @@ public class StringsAssignment {
 		
 		String word = "Birthday";
 		
+		System.out.println("");
 		
-	
+		System.out.println(word.toUpperCase());
+		
+		String word2 = "it's my party";
+		
+		System.out.println(word2.substring(0,4) + " a \"" + word2.substring(8).toUpperCase() + "\"");
+		
+		String word3 = "\"whose a whats it\"";
+		
+		System.out.println(word3.substring(1,6).toUpperCase() + "\nis " + word3.substring(9,13).toUpperCase()); 
+		
+		String word4 = "Every Way The Wind Blows";
+		
+		System.out.println(word4.substring(0,5).toLowerCase() + " " + word4.substring(6,9).toUpperCase() + word4.substring(9,13).toLowerCase() + " \\" + word4.substring(14,18).toUpperCase() + "\\ " + word4.substring(19).toLowerCase() + " 4");
 	}
 
 }
