57748d1faaafefb600348fc91a035c8b0915cf1f
jfaustino <jfaustino@HS-D210-19.MOHS.local>
2019-03-08 12:10:11 -0500

    3/8/19

---
+++ Lesson 3 - Strings/src/Numbers.java
@@ -0,0 +1,15 @@
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double weight = 155.3;
+		
+		weight = weight + 1.3;
+		
+		System.out.println(weight);
+	
+				
+	}
+
+}
--- Lesson 3 - Strings/src/StringsAssignment.java
+++ Lesson 3 - Strings/src/StringsAssignment.java
@@ -22,8 +22,6 @@ public class StringsAssignment {
 		
 		System.out.print("\"\\\\\"\\\"\" \n \t \\\"\\\"\"\\ \n \\\"\"\"\"\\");
 		
-		System.out.println("");
-		
 		String word = "Birthday";
 		
 		System.out.println("");
@@ -41,6 +39,32 @@ public class StringsAssignment {
 		String word4 = "Every Way The Wind Blows";
 		
 		System.out.println(word4.substring(0,5).toLowerCase() + " " + word4.substring(6,9).toUpperCase() + word4.substring(9,13).toLowerCase() + " \\" + word4.substring(14,18).toUpperCase() + "\\ " + word4.substring(19).toLowerCase() + " 4");
+		
+		String word5 = "Peter Piper Picked A Peck Of Pickled Peppers";
+		
+		System.out.println(word5.substring(19,20) + " " + word5.substring(21,36).toLowerCase() + " " + "\t" + word5.substring(37).toLowerCase() + " " + word5.substring(0,5) + "\t" + word5.substring(12,18).toLowerCase());
+		
+		String word6 = "word has 4 letters";
+		
+		System.out.println ("\"" + word6.substring(9,10) + "\"" + " " + word6.substring(11,18) + " " + word6.substring(5,8) + " " + "\"9\"" + " " + word6.substring(11,18));
+		
+		String word7 = "if SUBSTRING is used";
+		
+		String x = "SUBSTRING";
+		
+		System.out.println(word7.substring(3,12).toLowerCase() + " " + word7.substring(13,15) + " " + x.length());
+		
+		String word8 = "Jim, Jacky, Johny, Jill";
+		
+		String y = "Jim";
+		
+		String z = "Jacky";
+		
+		String a = "Johnny";
+		
+		String b = "Jill";
+		
+		System.out.println(word8.substring(0,3).toLowerCase() + "\\" + y.length() + "\n" + word8.substring(5,10).toUpperCase() + "\\" + z.length() + "\n" + word8.substring(12,17).toLowerCase() + "\\" + a.length() + "\n" + word8.substring(19,23).toUpperCase() + "\\" + b.length());
 	}
 
 }
